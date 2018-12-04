1a10ecfba3bd95c4ed3684e9ad3edf41f07aa712
Kimbugwe Simon Peter <39520700+kimbugp@users.noreply.github.com>
2018-12-04 16:09:00 +0300

    Update protectedRoute.js.js

--- src/__tests__/authTest/protectedRoute.js.js
+++ src/__tests__/authTest/protectedRoute.js.js
@@ -1,6 +1,8 @@
 import ProtectedRoute, { Authenticate } from "../../routes/protectedRoutes";
 import { shallow } from "enzyme";
 import React from "react";
+var jwt = require("jsonwebtoken");
+var token = jwt.sign({ user_id: "bar", exp: Date.now() }, "shhhhh");
 
 it("test invalid token at login", () => {
   expect(Authenticate("dfdjfdj")).toEqual(false);
@@ -15,4 +17,7 @@ it("test valid token at login but expired", () => {
 it("protected route renders correctly", () => {
   let component = shallow(<ProtectedRoute />);
   component.instance();
+});
+it("protected route renders correctly", () => {
+  expect(Authenticate(token)).toBeDefined();
 });
